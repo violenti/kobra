eaea9eba98c43b19157d2ff95e3c1ec650ac8500
david <rodolfo.fernandez@fravega.com.ar>
2022-07-31 23:32:20 -0300

    complete linter

--- kobra.py
+++ kobra.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
+"""System module."""
 
-#import socket
-import dns.resolver
-import dns.exception
 import time
 import argparse
 import sys
+import dns.resolver
+import dns.exception
 
-
-resolver = dns.resolver.Resolver()
 print (r"""
   _         _
  | |       | |
@@ -22,50 +20,65 @@ print (r"""
   LICENSE MIT
      """)
 
-
 def parse_args():
-    parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -i mydomain.txt")
+    """
+    function for argparse
+
+    """
+    parser = argparse.ArgumentParser(epilog='\tExample:\r\npython'
+                                     + sys.argv[0] +
+                                     "-i mydomain.txt")
     parser._optionals.title = "OPTIONS"
-    parser.add_argument('-i', '--input', help="file from will your list with domain",type=str, required=True)
-    parser.add_argument('-r','--resolver',help="dns name for resolver, for example 8.8.8.8", type=str, required=False)
-    parser.add_argument('-t','--type',help="type of dns record for finding", type=str,required=False)
+    parser.add_argument('-i', '--input', help="file from will your list with domain",
+                       type=str, required=True)
+    parser.add_argument('-r','--resolver',
+                        help="dns name for resolver,for example 8.8.8.8",
+                        type=str, required=False)
+    parser.add_argument('-t','--type',help="type of dns record for finding",
+                        type=str,required=False)
 
     return parser.parse_args()
 
 args = parse_args()
 
-
 def finddns(nameservers, domain):
+    """
+    function of support
+    """
+
     for data in nameservers :
         print (domain,"",data)
 
 
 def main():
+    """
+    function main
+    """
     filedns = args.input
     #output = args.output
     resolverdns = args.resolver
     typerecords = args.type
     nameservers= ''
     if typerecords is None:
-        print ("Please set the dns records")    
+        print ("Please set the dns records")
     else:
-       with open(filedns,encoding='utf-8') as f:
-           for line in f:
-               domain= line.strip()
-               time.sleep(3)
-               if resolverdns is None:
-                   try:
-                     nameservers = dns.resolver.resolve(domain, typerecords)
-                   except dns.exception.DNSException as e:
-                       print (domain,"","not register")
-               else:
-                  resolver=dns.resolver.Resolver(configure=False)
-                  resolver.nameservers = [resolverdns]
-                  try:
-                      nameservers = dns.resolver.resolver(domain,typerecords)
-                  except dns.exception.DNSException as e:
-                      print (domain,"","not register")
-               finddns(nameservers,domain)
+        with open(filedns,encoding='utf-8') as file:
+            for line in file:
+                domain= line.strip()
+                time.sleep(3)
+                if resolverdns is None:
+                    try:
+                        nameservers = dns.resolver.Resolver(domain, typerecords)
+                    except dns.exception.DNSException:
+                        print (domain,"","not register")
+                else:
+                    resolver=dns.resolver.Resolver(configure=False)
+                    resolver.nameservers = [resolverdns]
+                    try:
+                        nameservers = dns.resolver.Resolver(domain,typerecords)
+                    except dns.exception.DNSException:
+                        print (domain,"","not register")
+                finddns(nameservers,domain)
 
 
 if __name__ == '__main__':
